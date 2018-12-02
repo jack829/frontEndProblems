c9e0d05334b16b663569e3449125b77c6e9b5bea
Jack Peterson <jlpeterson829@gmail.com>
2018-12-02 14:03:16 -0700

    Add image loader with basic poc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import './style.css'
+import logo from './images/logo.png'
 
-const element = document.createElement('p')
-element.innerHTML = 'Hello'
-element.classList.add('hello')
-document.body.appendChild(element)
+function addText() {
+  const element = document.createElement('p')
+  element.innerHTML = 'Hello'
+  element.classList.add('hello')
+  return element
+}
+
+function addFavicon() {
+  const image = document.createElement('img')
+  image.height = 100
+  image.width = 100
+  image.src = logo
+  return image
+}
+
+document.body.appendChild(addText())
+document.body.appendChild(addFavicon())
